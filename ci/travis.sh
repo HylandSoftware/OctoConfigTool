ba227e70009c7f0429f434b768ac5ea3c884c32a
Alex Fuerst <fuersta.2013@gmail.com>
2019-07-22 13:01:26 -0400

    Fix travis.sh file

--- ci/travis.sh
+++ ci/travis.sh
@@ -8,7 +8,7 @@ CAKE_ARGS=""
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     CAKE_TASK=ci-publish
-	CAKE_ARGS="--nugetApiKey ${NUGET_API_KEY} --nugetPublishUrl ${NUGET_PUBLISH_URL}"
+	CAKE_ARGS="--nugetApiKey=${NUGET_API_KEY} --nugetPublishUrl=${NUGET_PUBLISH_URL}"
     #docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
     IMAGE_TAG=$(docker run -it --rm -v "$(pwd):/repo" gittools/gitversion-dotnetcore:linux-4.0.1 /repo /showvariable NuGetVersionV2)
